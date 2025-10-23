6aa59830bff2543f90bff4d7b88aea89261076f1
1307manasa <145106699+1307manasa@users.noreply.github.com>
2025-10-23 16:18:46 +0530

    Add files via upload

---
+++ tableof7.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class tableof7 {
+
+	public static void main(String[] args)
+	{
+		int num = 7;
+
+        for (int i = 1; i <= 10; i++) 
+        {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+
+	}
+
+}
